24a9dca0737ad50e3b8c85a66ad0cd9d359c7493
eunsu <songess@naver.com>
2024-06-26 17:51:56 +0900

    [ReFactor]: 로그인페이지에서 랜딩페이지로 가는 버튼 추가

--- src/app/(LoginSignupLayout)/login/page.tsx
+++ src/app/(LoginSignupLayout)/login/page.tsx
@@ -1,12 +1,21 @@
-// 승완
+'use client'
 import LoginForm from '@components/login/LoginForm';
 import OrSignup from '@components/login/OrSignup';
+import ArrowBackSVG from '@public/arrowBack.svg';
+import { useRouter } from 'next/navigation';
 export default function LoginPage() {
+  const router = useRouter();
   return (
     <>
       <p className="w-full h-[100px] min-h-[100px] flex justify-center items-end text-3xl font-bold mb-[50px]">
         CEOS 투표 로그인
       </p>
+      <ArrowBackSVG
+        className="absolute top-[20px] left-[40px] cursor-pointer"
+        onClick={() => {
+          router.push('/');
+        }}
+      />
       <LoginForm />
       <OrSignup />
     </>
