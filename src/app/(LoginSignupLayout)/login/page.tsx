03f4492b9db6f51a6dc7ff182dd621d7d3dda17d
eunsu <songess@naver.com>
2024-06-27 19:55:34 +0900

    [Fix]: 로그인페이지 뒤로가기 위치조정

--- src/app/(LoginSignupLayout)/login/page.tsx
+++ src/app/(LoginSignupLayout)/login/page.tsx
@@ -6,18 +6,18 @@ import { useRouter } from 'next/navigation';
 export default function LoginPage() {
   const router = useRouter();
   return (
-    <>
+    <div className="relative">
       <p className="w-full h-[100px] min-h-[100px] flex justify-center items-end text-3xl font-bold mb-[50px]">
         CEOS 투표 로그인
       </p>
       <ArrowBackSVG
-        className="absolute top-[20px] left-[40px] cursor-pointer"
+        className="absolute top-[20px] left-[10px] cursor-pointer"
         onClick={() => {
           router.push('/');
         }}
       />
       <LoginForm />
       <OrSignup />
-    </>
+    </div>
   );
 }
